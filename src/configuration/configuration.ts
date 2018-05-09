3e4cef3cb41c35f232ba7eeed0dd5ae15ed932ef
knives81 <maurizio.pinzi@gmail.com>
2018-05-09 08:31:31 +0100

    new version

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -6,7 +6,7 @@ export class Configuration {
     serverUrl : string;
     username : string;
     password : string;
-    appVersion = "1.8";
+    appVersion = "1.9";
 
     constructor(public storage: Storage) {
       console.log('Load configuration');
